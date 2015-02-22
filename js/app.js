5463d084c3ba188e7af591dfa4e1ab6150d1df9f
Ayesha Ilyas <ayesha.nizami@gmail.com>
2015-02-21 20:59:21 -0800

    All binding working, set infowindow and fix bounce

--- js/app.js
+++ js/app.js
@@ -1,61 +1,44 @@
+/* LOCATION MODEL*/
 var Locations = function () {
+  
   var self=this;
-  
- /* self.searchTerm = ko.observable('');
-  self.locations = ko.observableArray([{'name': 'Irvine', 'year' : 2012},
-                                        {'name':'Anaheim', 'year' : 2013},
-                                        {'name': 'Houston' ,'year' : 2010}
-                                        ]);
-
-  
-  
-  //IIFE to load data and set up locations
-  self.loadData = (function () {
-
-      self.locations.push({'name': 'Lahore', 'year':2009});
-  
-
-  })();
-    
-  //self.searchResult = ko.computed (function() {  
-    //console.log(self.locations());
-    //for (var y in self.locations())
-   // {
-      
-      //console.log(self.locations()[y]['name']);
-      //console.log(self.searchTerm());
-     // if (self.locations()[y]['name'] == self.searchTerm() ){
-       // console.log (self.locations()[y]['year']);
-     // }
-   // }
-
-
-   //},self);
-*/
-
 
   self.locations = ko.observableArray();
-  self.test1 = ko.observable(2);
-  //self.searchTerm = ko.observable('');
+  
 
   //IIFE to load data and set up locations
   self.loadData = (function () {
-    var foursquare="https://api.foursquare.com/v2/venues/explore?client_id=TCESTHYEP5B4JGEU2IHGJVXSQL3ONR0ZGML5DJY0OTEIJZ3X&client_secret=UBF0HHQEQG3K01KKTGXZLPGKBCPLKFK1VQFRBJ4GAMKKYDZ3&v=20130815&ll=33.669444,-117.823056&section=food&limit=10";  
-    $.getJSON(foursquare,
     
-    function(data) {
+    var foursquare="https://api.foursquare.com/v2/venues/explore?client_id=TCESTHYEP5B4JGEU2IHGJVXSQL3ONR0ZGML5DJY0OTEIJZ3X&client_secret=UBF0HHQEQG3K01KKTGXZLPGKBCPLKFK1VQFRBJ4GAMKKYDZ3&v=20130815&ll=33.669444,-117.823056&section=food&limit=10";  
+    
+    $.getJSON(foursquare,
+
+      function(data) {
       
         var places=data.response.groups[0].items;
-        //console.log(places);
+      
         for (var i=0; i<places.length; i++) {
-            self.locations.push({ 
-                'venue'  :data.response.groups[0].items[i].venue,
-                'name': data.response.groups[0].items[i].venue.name, 
-                'lat': data.response.groups[0].items[i].venue.location.lat ,
-                'lng':data.response.groups[0].items[i].venue.location.lng
-            });    
+          
+          var marker = new google.maps.Marker({
+                        position: { lat: data.response.groups[0].items[i].venue.location.lat , 
+                          lng : data.response.groups[0].items[i].venue.location.lng },
+                        title : data.response.groups[0].items[i].venue.name
+                      });
+          
+          var infoWindow = new google.maps.InfoWindow ( { 
+                            content : data.response.groups[0].items[i].venue.name
+                            });
+          
+          self.locations.push({ 
+              'venue' : data.response.groups[0].items[i].venue,
+              'name' : data.response.groups[0].items[i].venue.name, 
+              'marker': marker,
+              'infowindow' : infoWindow
+              //'lat': data.response.groups[0].items[i].venue.location.lat ,
+              //'lng': data.response.groups[0].items[i].venue.location.lng
+          });    
         }
-        //console.log(self.locations());
+        
       }).fail ( function (){
           console.log ("Error");
       });
@@ -64,74 +47,79 @@ var Locations = function () {
 
 }
 
+/*Location View Model*/
 var LocationsViewModel = function () {
 
   var self=this;
 
   self.location=ko.observable(new Locations());
 
-  //self.clickCounter =ko.observable(0);
+  self.searchTerm = ko.observable(''); 
 
-  /*self.mapOptions = {
-      //disableDefaultUI: true33.669444, -117.823056
-      center: { lat: 33.669444, lng: -117.823056},
-      zoom: 14
-    };
-  self.map =  new google.maps.Map(document.getElementById('map'), self.mapOptions);
-  self.map.setCenter({ lat: 33.669444, lng: -117.823056});
- */
- // self.displayList = ko.observableArray();
-
-  self.searchTerm = ko.observable('');
-
-  //this.location().loadData();
-  //console.log(self.location().locations());
-  //self.displayList = ko.observableArray([]);
   self.test=ko.observable(0);
+
+  /* MAP */
+  self.mapOptions = {
+      //disableDefaultUI: true
+      center: { lat: 33.669444, lng: -117.823056},
+      zoom: 12
+  };
+
+  self.map =  new google.maps.Map(document.getElementById('map'), self.mapOptions);
+
+  self.map.setCenter({ lat: 33.669444, lng: -117.823056});
+
+  /* Display Filtered List*/
   self.displayList = ko.computed ( function ( ) {
       
       var allPlaces=self.location().locations();
+      
       if (self.searchTerm() =='') { //display default list
-        return allPlaces;
+        
+        //setmap off all markers.
+        for (var i=0; i< allPlaces.length; i++)
+        {
+            allPlaces[i].marker.setMap(this.map);
 
+            google.maps.event.addListener ( allPlaces[i].marker, 'click', (function(allPlacesCopy) {
+                
+                return function () {
+                  //self.test(allPlacesCopy.name);
+                  allPlacesCopy.infowindow.open( self.map,this);
+
+                }
+              //console.log(allPlaces[i].name);
+                //allPlaces[i].infowindow.open( self.map,this);
+            })(allPlaces[i]) );
+
+        }
+        //console.log(self.location().locations());
+        return allPlaces;
+      
       }
 
-      var allPlaces=self.location().locations();
       var filteredList=[];
+      //search for places matching the search term
       for (var i=0; i<allPlaces.length; i++) {
-        if ( allPlaces[i]['name'].toLowerCase().indexOf(self.searchTerm().toLowerCase()) != -1 ) {
-          //if (i==2) {  
+        
+        if ( allPlaces[i]['name'].toLowerCase().indexOf(self.searchTerm().toLowerCase()) != -1 ) { 
+        
             filteredList.push(allPlaces[i]);
+        
         }
 
       }
       
-
-      return filteredList;
+      return filteredList; //display list will be this filtered list
 
     },self);
 
     self.displayInfo = function () {
-      self.test(this.lat);
+      //self.test(this.lat);
+      this.marker.setAnimation(google.maps.Animation.BOUNCE);//.setAnimation(google.maps.Animation.BOUNCE);
       //console.log(this.lat);
 
     };
-
-  
-
-  //console.log(self.displayList().pop());
-  
-
-
-
-  //create a displayList which will default to the locations[] initially but filter based on search results
-  
-  /*self.displayList = ko.observableArray ([{'name': 'Irvine', 'year' : 2012},
-                                        {'name':'Anaheim', 'year' : 2013},
-                                        {'name': 'Houston' ,'year' : 2010}
-                                        ]); //initially contains all data
-  *///console.log(self.displayList());
-
 
 }
 
